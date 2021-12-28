870d8d9e0f1b86489145c4f03ad9905118dc50d9
William Lahti <wilahti@gmail.com>
2021-12-28 11:37:46 -0800

    add support for autosave webhook

--- src/discontented/web-service/webhook-controller.ts
+++ src/discontented/web-service/webhook-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, WebEvent, Body, Response } from "@alterior/web-server";
-import { CF_TOPIC_ENTRY_PUBLISH, CF_TOPIC_ENTRY_UNPUBLISH, Context, CfEntry, CF_TOPIC_ENTRY_SAVE, CF_TOPIC_ENTRY_DELETE, CF_TOPIC_ENTRY_ARCHIVE, CF_TOPIC_ENTRY_UNARCHIVE, CF_TOPIC_ENTRY_CREATE } from "../common";
+import { CF_TOPIC_ENTRY_PUBLISH, CF_TOPIC_ENTRY_UNPUBLISH, CF_TOPIC_ENTRY_AUTO_SAVE, Context, CfEntry, CF_TOPIC_ENTRY_SAVE, CF_TOPIC_ENTRY_DELETE, CF_TOPIC_ENTRY_ARCHIVE, CF_TOPIC_ENTRY_UNARCHIVE, CF_TOPIC_ENTRY_CREATE } from "../common";
 import * as bodyParser from 'body-parser';
 import { HttpError } from "@alterior/common";
 import { PullService } from "../service/pull.service";
@@ -29,7 +29,7 @@ export class CfWebhookController {
         if (!entry)
             return Response.badRequest();
 
-        let cfTopic = WebEvent.request.headers['x-contentful-topic'];
+        let cfTopic = <string>WebEvent.request.headers['x-contentful-topic'];
 
         if (cfTopic === CF_TOPIC_ENTRY_UNPUBLISH) {
             await this.pullService.setEntryUnpublished(entry);
@@ -48,8 +48,10 @@ export class CfWebhookController {
 
         // The rest is for create/save/publish
 
-        if (cfTopic !== CF_TOPIC_ENTRY_PUBLISH && cfTopic !== CF_TOPIC_ENTRY_SAVE && cfTopic !== CF_TOPIC_ENTRY_CREATE)
+        if ([CF_TOPIC_ENTRY_AUTO_SAVE, CF_TOPIC_ENTRY_PUBLISH, CF_TOPIC_ENTRY_SAVE, CF_TOPIC_ENTRY_CREATE].includes(cfTopic)) {
+            console.log(`Unhandled webhook topic: ${cfTopic}, skipping!`);
             return;
+        }
         
         if (!this.context.schema) {
             console.error(`Cannot import content from webhook: No schema loaded`);
